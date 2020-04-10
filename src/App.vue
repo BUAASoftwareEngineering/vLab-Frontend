021f44e1282cb72be3853819189cca9e8a8a89c1
syncline0605 <hanjing19990713@163.com>
2020-04-10 13:26:04 +0800

    feat:美化顶端栏按钮与下拉菜单样式
    尚未解决滚动条问题

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@
     }
     .layout-header-bar{
         background: #fff;
-        box-shadow: 0 1px 1px rgba(0,0,0,.1);
     }
     .demo-split{
         height: 200px;
@@ -19,13 +18,45 @@
     }
     .ivu-btn{
         border-radius: 0px;
-        color:#464e57;background-color:#464e57;border-color:#464e57
+        color:#fff;background-color:#464e57;border-color:#464e57;
+		border: 0px solid transparent;
+        padding: 6px 16px 6px;
+        margin:-3px;
+    }
+	.ivu-btn:hover{
+      background-color: dimgrey;
+    }
+    .ivu-dropdown-rel{
+        padding: -3px;
+    }
+    .ivu-dropdown-menu{
+        max-height: 400px;
+    }
+    .ivu-dropdown-item{
+     padding: 0px 16px;
+      height: 20px;
+        float: none;
+    }
+    .ivu-dropdown-item-divided{
+      height: 20px;
+      margin-top: 4px;
+      margin-bottom: 7px;
+      padding: 0px 16px 0px;
+      line-height: 20px;
+      border-top: 2px solid #e8eaec;
+        float: none;
     }
     .ivu-layout-header{
         height:36px;
         line-height:36px;
         padding:0;
-        
+        background-color: #464e57;
+        margin: 0;
+    }
+	.ivu-select-dropdown{
+      border-radius: 0px;
+      margin-top: 0px;
+      max-height: 200px;
     }
 </style>
 
