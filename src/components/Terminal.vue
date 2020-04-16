37f1f0d3289e5ae179ca3bebc34fac917ffa360c
李沛熙 <17231162@buaa.edu.cn>
2020-04-16 21:09:52 +0800

    [web-terminal v1.0] initial web-terminal

---
+++ src/components/Terminal.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+    <div>
+        <button @click="fitin()">fitin</button>
+    </div>
+    <div id="xterm" class="xterm" />
+    </div>
+</template>
+
+<script>
+import '../../node_modules/xterm/css/xterm.css'
+import { Terminal } from 'xterm'
+import { FitAddon } from 'xterm-addon-fit'
+import { AttachAddon } from 'xterm-addon-attach'
+
+export default {
+  name: 'Xterm',
+  props: {
+    socketURI: {
+      type: String,
+      default: 'ws://127.0.0.1:4001'
+    },
+  },
+  mounted() {
+    this.initSocket()
+  },
+  beforeDestroy() {
+    this.socket.close()
+    this.term.dispose()
+  },
+  methods: {
+    initTerm() {
+      const term = new Terminal({
+        fontSize: 14,
+        cols: 92,
+        rows: 50,
+        cursorBlink: true,
+        scrollback: 800, 
+        tabStopWidth: 8, 
+        screenKeys: true
+      });
+      const attachAddon = new AttachAddon(this.socket);
+      const fitAddon = new FitAddon();
+      term.loadAddon(attachAddon);
+      term.loadAddon(fitAddon);
+      term.open(document.getElementById('xterm'));
+      fitAddon.fit();
+      term.focus();
+      term.fit()
+      this.term = term
+    },
+    fitin() {
+        this.socket.send(new TextEncoder().encode("\x00ls\r"))
+    },
+    initSocket() {
+      this.socket = new WebSocket(this.socketURI);
+      this.socketOnClose();
+      this.socketOnOpen();
+      this.socketOnError();
+    },
+    socketOnOpen() {
+      this.socket.onopen = () => {
+        // 链接成功后
+        this.initTerm()
+      }
+    },
+    socketOnClose() {
+      this.socket.onclose = () => {
+        // console.log('close socket')
+      }
+    },
+    socketOnError() {
+      this.socket.onerror = () => {
+        // console.log('socket 链接失败')
+      }
+    }
+  }
+}
+</script>
