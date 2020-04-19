5be21bfa907d588bf55eaee77b6bc8a2631c35b3
李沛熙 <17231162@buaa.edu.cn>
2020-04-19 16:17:50 +0800

    [web-terminal v1.0] test Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Terminal from '@/components/Terminal'
+import MainPage from '@/components/MainPage'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Terminal',
       component: Terminal
+    },
+    {
+      path: '/test',
+      name: 'MainPage',
+      component: MainPage
     }
   ]
 })
