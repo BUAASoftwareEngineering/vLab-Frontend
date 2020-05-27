9fd47cc1f0e8ba40e8c38b06d94a5b555779ece2
田周瑾 <1017901779@qq.com>
2020-05-27 21:33:58 +0800

    feat: 增加保存到已有项目的功能 #125

--- src/components/MyDraft.vue
+++ src/components/MyDraft.vue
@@ -30,6 +30,38 @@
             <Button type="success" v-on:click="login">登录</Button>
           </div>
         </Modal>
+        <Modal
+          v-model="saveDraftModal"
+          title="将草稿保存到项目"
+          width="350"
+        >
+          <div style="text-align:center">
+            <Select
+              v-model="selectProjectID"
+              style="width:300px;text-align:left"
+              placeholder="请选择已有项目"
+            >
+              <Option
+                v-for="item in projects"
+                :value="item.projectId"
+                :key="item.projectId"
+              >{{ item.name }}</Option>
+            </Select>
+            <!--
+            <br />
+            <br />
+            <Input type="text" placeholder="新建项目" v-model="newProjectame" style="width: 300px;" />
+            -->
+            <br />
+            <br />
+            <Input type="text" placeholder="请输入文件名" v-model="draftName" style="width: 300px" />
+          </div>
+          
+          <div slot="footer" style="text-align:center">
+            <Button v-on:click="saveToProject">保存</Button>
+          </div>
+          
+        </Modal>
         <!--左侧菜单栏-->
         <Sider width="75">
           <Button
@@ -51,7 +83,13 @@
                 <Button type="primary" style="margin-top:5px;" @click title="运行">运行</Button>
               </Col>
               <Col :span="12" style="text-align:center">
-                <Button type="primary" style="margin-top:5px;" @click title="保存到项目">保存到项目</Button>
+                <Button
+                  type="primary"
+                  style="margin-top:5px;"
+                  @click="saveDraft"
+                  title="保存到项目"
+                  :disabled = "saving"
+                >保存到项目</Button>
               </Col>
             </Row>
           </Layout>
@@ -80,9 +118,15 @@ export default {
   data() {
     return {
       username: "",
+      language: "",
       notLogin: false,
       loginUsername: "",
-      loginPassword: ""
+      loginPassword: "",
+      saveDraftModal: false,
+      draftName: "",
+      saving: false,
+      selectProjectID: "",
+      projects: []
     };
   },
   beforeCreate: function() {
@@ -118,15 +162,128 @@ export default {
     cancelLogin() {
       var _this = this;
       _this.$router.push("/");
+    },
+    saveDraft() {
+      this.draftName = "";
+      this.saveDraftModal = true;
+    },
+    saveToProject() {
+      var _this = this;
+      var fileContent = "";
+      _this.$Notice.open({
+        title: '正在保存中...',
+        desc: '',
+        name: "saveNotice",
+        duration : 0
+      });
+      _this.saveDraftModal = false;
+      _this.saving = true;
+      console.log(_this.selectProjectID);
+      console.log(_this.draftName);
+      api.project_enter(_this.selectProjectID, function(response) {
+        if (response.code == 0) {
+          console.log("enterOk");
+          var timer = setInterval(function() {
+            api.file_new(
+              _this.selectProjectID,
+              "/code/" + _this.draftName,
+              function(response) {
+                if (response.code == 0) {
+                  clearInterval(timer);
+                  console.log("newok");
+                  api.file_update(
+                    _this.selectProjectID,
+                    "/code/" + _this.draftName,
+                    new Buffer(fileContent),
+                    function(response) {
+                      if (response.code == 0) {
+                        console.log("saveOK");
+                        api.project_exit(_this.selectProjectID, function(
+                          response
+                        ) {
+                          if (response.code == 0) {
+                            _this.$Notice.open({
+                              title: '保存成功',
+                              desc: ''
+                            });
+                            _this.saveDraftModal = false;
+                            _this.saving = false;
+                            console.log("exitOK");
+                          } else if (response.code == -101) {
+                            console.log("cookie验证失败");
+                          } else if (response.code == -102) {
+                            console.log("权限不足");
+                          } else {
+                            console.log("未知错误");
+                          }
+                        });
+                      } else if (response.code == -101) {
+                        _this.$Message.error("cookie验证失败");
+                        _this.notLogin = true;
+                      } else if (response.code == -102) {
+                        _this.$Message.error("权限不足");
+                      } else {
+                        _this.$Message.error("未知错误");
+                      }
+                    }
+                  );
+                } else if (response.code == -101) {
+                  _this.$Message.error("cookie验证失败");
+                  _this.notLogin = true;
+                } else if (response.code == -102) {
+                  _this.$Message.error("权限不足");
+                } else if (response.code == -301) {
+                  console.log("exist");
+                  _this.$Message.error("文件名已存在");
+                } else if (response.code == 500) {
+                } else {
+                  _this.$Message.error("未知错误");
+
+                }
+              }
+            );
+          }, 3000);
+        } else if (response.code == -101) {
+          _this.$Message.error("cookie验证失败");
+        } else if (response.code == -102) {
+          _this.$Message.error("权限不足");
+        } else {
+          _this.$Message.error("未知错误");
+        }
+      });
     }
   },
   mounted() {
     var _this = this;
+    this.language = this.$route.query.language;
     api.user_info(function(response) {
       if (response.code != 0) {
         _this.notLogin = true;
       } else {
         _this.username = response.data.name;
+        api.project_info(function(response) {
+          console.log(response);
+          if (response.code == 0) {
+            _this.projects.splice(0, _this.projects.length);
+            var projects = response.data;
+            for (var i = 0; i < projects.length; i++) {
+              if (projects[i].imageType == api.CPP && _this.language == "cpp") {
+                _this.projects.push(projects[i]);
+              }
+              if (
+                projects[i].imageType == api.PYTHON3 &&
+                _this.language == "python"
+              ) {
+                _this.projects.push(projects[i]);
+              }
+            }
+          } else if (response.code == -101) {
+            _this.$Message.error("cookie验证失败");
+            _this.notLogin = true;
+          } else {
+            _this.$Message.error("未知错误");
+          }
+        });
       }
     });
   },
@@ -171,6 +328,13 @@ span:hover {
   color: #f5f7f9;
   background-color: #515a6eec;
   border-color: #515a6e;
+  border: 0px solid transparent;
+}
+.MyLightDraftBody .ivu-btn:disabled {
+  border-radius: 3px;
+  color: #f5f7f9;
+  background-color: #515a6e62;
+  border-color: #515a6e60;
   border: 0px solid transparent;
 }
 </style>
